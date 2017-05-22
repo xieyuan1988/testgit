9f95889c7c726a4cbcf18d578d0f4beca8418db1
xieyuan <214876384@qq.com>
2017-05-22 15:35:24 +0800

    提交

--- src/main/java/cn/com/trade365/Test.java
+++ src/main/java/cn/com/trade365/Test.java
@@ -6,6 +6,6 @@ package cn.com.trade365;
 public class Test {
     public static void  main(String[] args){
         System.out.println("添加了些新功能");
-        System.out.println("V1.2.0新功能");
+        System.out.println("V1.2.0新功能0");
     }
 }
