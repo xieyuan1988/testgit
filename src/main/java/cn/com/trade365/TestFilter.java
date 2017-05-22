476ab3a1c83c83c1a191b47ffeeb2f5257f6173d
xieyuan <214876384@qq.com>
2017-05-22 15:35:57 +0800

    提交

---
+++ src/main/java/cn/com/trade365/TestFilter.java
@@ -0,0 +1,32 @@
+package cn.com.trade365;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class TestFilter implements Filter{
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2)
+			throws IOException, ServletException {
+		System.out.println("呵呵");
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
