64a757b2c97ffebc8729262d22ca430ad2566bc5
xieyuan <214876384@qq.com>
2017-05-22 15:42:05 +0800

    有改动

--- src/main/java/cn/com/trade365/Test.java
+++ src/main/java/cn/com/trade365/Test.java
@@ -7,5 +7,6 @@ public class Test {
     public static void  main(String[] args){
         System.out.println("添加了些新功能");
         System.out.println("V1.2.0新功能0");
+        System.out.println("有改动");
     }
 }
